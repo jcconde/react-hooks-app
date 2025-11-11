8ee725d3e0b0b1d7246f2c81fe3c243cd95da133
Juanca <juancarlosc@onetree.com>
2025-11-11 12:17:10 -0400

    Add `TrafficLightWithEffect` component with countdown logic using `useEffect` and integrate it as the root component

---
+++ src/02-useEffect/TrafficLightWithEffect.tsx
@@ -0,0 +1,54 @@
+import { useEffect, useState } from "react";
+
+const colors = {
+  red: 'bg-red-500 animate-pulse',
+  yellow: 'bg-yellow-500 animate-pulse',
+  green: 'bg-green-500 animate-pulse',
+};
+
+// type TrafficLightColor = 'red' | 'yellow' | 'green';
+type TrafficLightColor = keyof typeof colors;
+
+
+export const TrafficLightWithEffect = () => {
+  const [light, setLight] = useState<TrafficLightColor>('red');
+  const [countdown, setCountDown] = useState(5);
+
+  useEffect(() => {
+    console.log({countdown});
+
+    const intervalId = setInterval(() => {
+      console.log('Countdown:', countdown);
+      setCountDown((prev) => prev - 1);
+    }, 1000);
+
+    return () => {
+      console.log('Cleanup effect');
+      clearInterval(intervalId);
+    }
+
+  }, [countdown]);
+
+  return (
+    <div
+      className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
+      <div className="flex flex-col items-center space-y-8">
+        <h1 className="text-white text-3xl font-thin">Semáfor con useEffect</h1>
+        <h2 className="text-white text-xl">Countdown {countdown}</h2>
+
+        <div className={`w-32 h-32 ${
+          light == 'red' ? colors[light] : 'bg-gray-500'
+        } rounded-full`}
+        ></div>
+        <div className={`w-32 h-32 ${
+          light == 'yellow' ? colors[light] : 'bg-gray-500'
+        } rounded-full`}
+        ></div>
+        <div className={`w-32 h-32 ${
+          light == 'green' ? colors[light] : 'bg-gray-500'
+        } rounded-full`}
+        ></div>
+      </div>
+    </div>
+  );
+};
