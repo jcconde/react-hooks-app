952aa3bb7644d72d34ab299fa99b91a30919c912
Juanca <juancarlosc@onetree.com>
2025-11-11 11:25:08 -0400

    Add `TrafficLight` component with basic UI and integrate it as the root component.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HooksApp } from "./HooksApp.tsx";
 import './index.css'
+import { TrafficLight } from "./01-useState/TrafficLight.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HooksApp/>
+    <TrafficLight/>
   </StrictMode>,
 )
