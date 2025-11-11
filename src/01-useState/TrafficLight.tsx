acbb9fb8fb3deb2d06a46f21dd43a0a889cd359e
Juanca <juancarlosc@onetree.com>
2025-11-11 11:44:44 -0400

    Add interactive state management to `TrafficLight` component with dynamic light switching

--- src/01-useState/TrafficLight.tsx
+++ src/01-useState/TrafficLight.tsx
@@ -1,24 +1,57 @@
+import { useState } from "react";
+
 export const TrafficLight = () => {
+  const [light, setLight] = useState<string>('red');
+
+  const handleColorChange = (color: string) => {
+    // setLight(color);
+    setLight((prev) => {
+      console.log(prev);
+      return color;
+    });
+  };
+
+  const colors = {
+    red: 'bg-red-500 animate-pulse',
+    yellow: 'bg-yellow-500 animate-pulse',
+    green: 'bg-green-500 animate-pulse',
+  };
+
   return (
     <div
       className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
       <div className="flex flex-col items-center space-y-8">
-        <div className="w-32 h-32 bg-red-500 rounded-full"></div>
-        <div className="w-32 h-32 bg-yellow-500 rounded-full"></div>
-        <div className="w-32 h-32 bg-green-500 rounded-full"></div>
+        <div className={`w-32 h-32 ${
+          light == 'red' ? colors[light] : 'bg-gray-500'
+        } rounded-full`}
+        ></div>
+        <div className={`w-32 h-32 ${
+          light == 'yellow' ? colors[light] : 'bg-gray-500'
+        } rounded-full`}
+        ></div>
+        <div className={`w-32 h-32 ${
+          light == 'green' ? colors[light] : 'bg-gray-500'
+        } rounded-full`}
+        ></div>
 
         {/* Botón para cambiar el estado de la luz */}
         <div className="flex gap-2">
           <button
-            className="bg-red-500 text-white px-4 py-2 rounded-md cursor-pointer">
+            className="bg-red-500 text-white px-4 py-2 rounded-md cursor-pointer"
+            onClick={() => handleColorChange('red')}
+          >
             Rojo
           </button>
           <button
-            className="bg-yellow-500 text-white px-4 py-2 rounded-md cursor-pointer">
+            className="bg-yellow-500 text-white px-4 py-2 rounded-md cursor-pointer"
+            onClick={() => handleColorChange('yellow')}
+          >
             Amarillo
           </button>
           <button
-            className="bg-green-500 text-white px-4 py-2 rounded-md cursor-pointer">
+            className="bg-green-500 text-white px-4 py-2 rounded-md cursor-pointer"
+            onClick={() => handleColorChange('green')}
+          >
             Verde
           </button>
         </div>
