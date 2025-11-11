70f6bec83b1eef53aaa3fcdfab5499c1ad601955
Juanca <juancarlosc@onetree.com>
2025-11-11 15:36:27 -0400

    Add `TrafficLightWithHook` component using custom `useTrafficLight` hook and integrate it as the root component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect";
+import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TrafficLightWithEffect/>
+    <TrafficLightWithHook/>
   </StrictMode>,
 )
